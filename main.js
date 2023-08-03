a95a6722ccf95fd07b61b568384bd3f4f78634e0
Victor Badanau <126888251+theinvestigattor@users.noreply.github.com>
2023-08-03 09:25:05 +0300

    add minimax algorithm

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const gameBoard = document.querySelector(".game-board");
 const restart = document.querySelector(".restart");
 const tokenSelector = document.getElementById("token-selector");
 const levelSelector = document.querySelector("#difficulty");
-const level = document.querySelector("#difficulty").value;
 
 function GameBoard() {
   const rows = 3;
@@ -58,37 +57,79 @@ function GameController() {
     return ["o", "x"];
   }
 
-  function availableCells(board) {
+  function winning(board, player) {
+    if (
+      (board[0] == player && board[1] == player && board[2] == player) ||
+      (board[3] == player && board[4] == player && board[5] == player) ||
+      (board[6] == player && board[7] == player && board[8] == player) ||
+      (board[0] == player && board[3] == player && board[6] == player) ||
+      (board[1] == player && board[4] == player && board[7] == player) ||
+      (board[2] == player && board[5] == player && board[8] == player) ||
+      (board[0] == player && board[4] == player && board[8] == player) ||
+      (board[2] == player && board[4] == player && board[6] == player)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function getCurrentBoard(board) {
     let emptyRows = [];
     let emptyColumns = [];
+    let gameBoard = [];
+    let emptyCellIndexes = [];
     for (let i = 0; i < board.length; i++) {
       for (let j = 0; j < board.length; j++) {
         if (board[i][j].getValue() === "") {
           emptyRows.push(i);
           emptyColumns.push(j);
+          emptyCellIndexes.push(3 * i + j);
+          gameBoard.push(3 * i + j);
+        } else {
+          gameBoard.push(board[i][j].getValue());
         }
       }
     }
-    return [emptyRows, emptyColumns];
+    return [emptyRows, emptyColumns, emptyCellIndexes, gameBoard];
   }
 
   function computerChoice(board) {
-    const cells = availableCells(board);
+    const level = document.querySelector("#difficulty").value;
+    const matrix = [
+      [0, 0],
+      [0, 1],
+      [0, 2],
+      [1, 0],
+      [1, 1],
+      [1, 2],
+      [2, 0],
+      [2, 1],
+      [2, 2],
+    ];
+    let currentBoard = getCurrentBoard(board);
+    let boardArray = currentBoard[3];
+    let human = getToken()[0];
+    let computer = getToken()[1];
     let rowIndex;
     let columnIndex;
     if (level == "easy") {
-      let randomCell = Math.floor(Math.random() * cells[0].length);
-      rowIndex = cells[0][randomCell];
-      columnIndex = cells[1][randomCell];
+      let randomCell = Math.floor(Math.random() * currentBoard[0].length);
+      rowIndex = currentBoard[0][randomCell];
+      columnIndex = currentBoard[1][randomCell];
     } else if (level == "medium") {
     } else if (level == "hard") {
     } else if (level == "impossible") {
+      let choice = minimax(boardArray, computer);
+      console.log(choice);
+      rowIndex = matrix[choice.index][0];
+      columnIndex = matrix[choice.index][1];
     }
     if (rowIndex == undefined) return;
     return [rowIndex, columnIndex];
   }
 
-  function playerMove(row, column) {
+  function humanMove(row, column) {
     gameBoard.placeToken(row, column, getToken()[0]);
   }
 
@@ -103,62 +144,81 @@ function GameController() {
     computerMove(board);
   }
 
-  function row(board, token) {
-    let array;
-    if (board == undefined || token == undefined) return;
-    array = board
-      .map((row) => row.filter((column) => column.getValue() == token))
-      .filter((row) => row.length === 3);
-    return array;
-  }
-  function column(board, token) {
-    let array;
-    if (board == undefined || token == undefined) return;
-    array = board[0]
-      .map((col, i) => board.map((row) => row[i]))
-      .map((row) => row.filter((column) => column.getValue() == token))
-      .filter((row) => row.length === 3);
-    return array;
-  }
-
-  function diagonal(board, token) {
-    if (board == undefined || token == undefined) return;
-    const length = board.length;
-    let firstDiagonal = [];
-    let secondDiagonal = [];
-    for (let i = 0; i < length; i++) {
-      firstDiagonal.push(board[i][i]);
-      secondDiagonal.push(board[i][length - i - 1]);
-    }
-    let diagonals = [firstDiagonal, secondDiagonal];
-    let array = diagonals
-      .map((row) => row.filter((column) => column.getValue() == token))
-      .filter((row) => row.length === 3);
-    return array;
-  }
-
-  function winner(board, win, lose) {
-    let xRow = row(board, "x");
-    let xColumn = column(board, "x");
-    let xDiagonal = diagonal(board, "x");
-    let oRow = row(board, "o");
-    let oColumn = column(board, "o");
-    let oDiagonal = diagonal(board, "o");
-    let player = getToken()[0];
-    let x = xRow.length !== 0 || xColumn.length !== 0 || xDiagonal.length !== 0;
-    let o = oRow.length !== 0 || oColumn.length !== 0 || oDiagonal.length !== 0;
-    if ((x && player === "x") || (o && player === "o")) {
+  function getWinner(board, win, lose, draw) {
+    let emptyCellIndexes = getCurrentBoard(board)[2];
+    let gameBoard = getCurrentBoard(board)[3];
+    let human = getToken()[0];
+    let computer = getToken()[1];
+    if (winning(gameBoard, human)) {
       win();
-    } else if ((x && player === "o") || (o && player === "x")) {
+    } else if (winning(gameBoard, computer)) {
       lose();
+    } else if (emptyCellIndexes.length == 0) {
+      draw();
     }
-    return;
+  }
+
+  function getEmptyIndexes(board) {
+    let array = [];
+    for (let i = 0; i < board.length; i++) {
+      if (typeof board[i] == "number") {
+        array.push(i);
+      }
+    }
+    return array;
+  }
+
+  function minimax(board, player) {
+    let empty = getEmptyIndexes(board);
+    let human = getToken()[0];
+    let computer = getToken()[1];
+    if (winning(board, human)) {
+      return { score: -10 };
+    } else if (winning(board, computer)) {
+      return { score: 10 };
+    } else if (empty.length === 0) {
+      return { score: 0 };
+    }
+    let moves = [];
+    for (let i = 0; i < empty.length; i++) {
+      let move = {};
+      move.index = board[empty[i]];
+      board[empty[i]] = player;
+      if (player === computer) {
+        let result = minimax(board, human);
+        move.score = result.score;
+      } else {
+        let result = minimax(board, computer);
+        move.score = result.score;
+      }
+      board[empty[i]] = move.index;
+      moves.push(move);
+    }
+    let bestMove;
+    if (player === computer) {
+      let bestScore = -10000;
+      for (let i = 0; i < moves.length; i++) {
+        if (moves[i].score > bestScore) {
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    } else {
+      let bestScore = 10000;
+      for (let i = 0; i < moves.length; i++) {
+        if (moves[i].score < bestScore) {
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    }
+    return moves[bestMove];
   }
 
   return {
-    winner,
+    getWinner,
     firstMove,
-    playerMove,
+    humanMove,
     computerMove,
     getBoard: gameBoard.getBoard,
     reset: gameBoard.reset,
@@ -202,25 +262,31 @@ function ScreenController() {
     const selectedColumn = e.target.dataset.column;
     let token = e.target.textContent;
     if (winner || token || !selectedRow) return;
-    game.playerMove(selectedRow, selectedColumn);
+    game.humanMove(selectedRow, selectedColumn);
     updateScreen();
-    game.winner(board, win, lose);
+    game.getWinner(board, win, lose, draw);
     if (winner) return;
     game.computerMove(board);
     setTimeout(updateScreen, 150);
-    game.winner(board, win, lose);
+    game.getWinner(board, win, lose, draw);
   }
 
   function win() {
     winner = 1;
     winModal.textContent = "You Win!";
-    openWinModal();
+    setTimeout(openWinModal, 150);
   }
-  
+
   function lose() {
     winner = 1;
     winModal.textContent = "You Lose!";
-    openWinModal();
+    setTimeout(openWinModal, 150);
+  }
+
+  function draw() {
+    winner = 1;
+    winModal.textContent = "It's a Draw!";
+    setTimeout(openWinModal, 150);
   }
 
   function reset() {
